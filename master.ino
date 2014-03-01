dd50beee094bc575cce57688a903c47e8ecfa980
fredthekid <fredthekid@gmail.com>
2014-03-01 01:54:22 -0800

    Update master.ino

--- master.ino
+++ master.ino
@@ -1,6 +1,29 @@
-//Master Uno, Remote
+//Remote (Master Bluetooth)
 
-//configure bluetooths
-Serial.print("$$$"); //enter command mode
-Serial.println("SM,1"); // set to master mode
-Serial.println("I,10");
+#include <SoftwareSerial.h>
+
+SoftwareSerial Master(9,10); // 9 = RX, 10 = TX
+
+void configbluetooth()
+{
+  Serial.begin(115200); //serial between computer and uno
+
+  Serial.println("Waiting for computer to UNO connection...");
+  while(!Serial){;} //wait for connection
+  Serial.println("Connected!");
+
+  Master.begin(9600)
+
+
+  Serial.print("$$$"); //enter command mode
+  Serial.println("SM,1"); // set to master mode
+  Serial.println("I,10"); 
+
+
+
+  Serial.println("SR,asdf"); //store slave address
+  Serial.println("C"); //connect to slave
+
+
+  Serial.println("---");//exit command mode
+}
