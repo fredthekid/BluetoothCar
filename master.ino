e657301d8a6caa8e508aa0288987713d5f2af80e
fredthekid <fredthekid@gmail.com>
2014-03-01 02:12:27 -0800

    Update master.ino

--- master.ino
+++ master.ino
@@ -4,26 +4,35 @@
 
 SoftwareSerial Master(9,10); // 9 = RX, 10 = TX
 
-void configbluetooth()
+void btinit()
 {
+  boolean test = true; //used for t
+  
+  //init comp to uno serial
   Serial.begin(115200); //serial between computer and uno
-
-  Serial.println("Waiting for computer to UNO connection...");
+  Serial.println("Initializing computer to UNO connection...");
   while(!Serial){;} //wait for connection
-  Serial.println("Connected!");
-
-  Master.begin(9600)
-
-
-  Serial.print("$$$"); //enter command mode
-  Serial.println("SM,1"); // set to master mode
-  Serial.println("I,10"); 
-
-
-
+  Serial.println("Connected");
+  
+  //init uno to bt serial
+  Master.begin(9600); //serial between uno and bluetooth
+  Serial.println("Initializing master...");
+  while(!Master){;} //wait for connection
+  Serial.println("Connected");
+  
+  //config bluetooth for connection
+  Master.print("$$$"); //enter command mode
+  delay(50);
+  Master.println("SM,1"); // set to master mode
+  delay(50);
+  Master.println("I,10"); // find slave
+  delay(50);
+  
+  
+  
   Serial.println("SR,asdf"); //store slave address
   Serial.println("C"); //connect to slave
-
-
+  
+  
   Serial.println("---");//exit command mode
 }
