995e563b16412c2317877856e49476bab912293e
pratiekpathak <pratiek.pathak@gmail.com>
2014-03-02 16:30:19 -0800

    Update slave.ino

--- slave.ino
+++ slave.ino
@@ -1,5 +1,3 @@
-//#include <Servo.h>
-//Servo steer;
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial (5, 6);
 
@@ -7,41 +5,51 @@ const int front_LED [2] = {7, 8};
 const int back_LED [2] = {12, 13};
 
 const int buzzerPin = 3;
+
 void setup(){
- mySerial.begin(9600);
- Serial.begin(115200);
- pinMode(3, OUTPUT);
- //steer.attach(3);
+
+   mySerial.begin(9600);
+   Serial.begin(115200);
+   
+   pinMode(3, OUTPUT);
    pinMode(7,OUTPUT);
    pinMode(8,OUTPUT);
    pinMode(9,OUTPUT);
    pinMode(10,OUTPUT);
    pinMode(11,OUTPUT);
    pinMode(12,OUTPUT);
+   
+   TCCR2A  =0b11000010;
+   TCCR2B  =0b00001101;
+   OCR2A   =188;
+
   TCCR1A	= 0b11110010;		// Clear OC1A/OC1B on compare match, set OC1A/OC1B at BOTTOM, Fast PWM mode
   TCCR1B	= 0b00011010;		// input noise canceler, input capture rising edge, 1 prescale
   ICR1	= 42000;     					// set period of PWM signal
-OCR1A	= 3000;					// set high time of output PWM signal
-OCR1B	= 3000;					// set high time of output PWM signal
+  
+  OCR1A	= 3000;					// set high time of output PWM signal
+  OCR1B	= 3000;					// set high time of output PWM signal
 }
 
 void loop(){
+
   char Reg_value= mySerial.read();
-  
   Serial.println(Reg_value, BIN);
-  char accel = Reg_value&0x03;
-  //char steer_value = Reg_value & 0xF8;
-  //steer_value >> 3;
-  //steer.write(map(steer_value, 0, 31, 0, 179));
   
-  switch(accel){
-    case 1: Forward();
-            break;
-    case 2: Reverse();
-            break;
-    default:Stop();
-  }
-  
+   char accel = Reg_value&0x03;
+   switch(accel)
+     {
+       case 1: Forward();
+               break;
+       case 2: Reverse();
+               break;
+       default:Stop();
+     }
+
+   char steer = Reg_value & 0x0F8;
+   steer>>3;
+   OCR2A = map(steer, 0, 31, 136, 230);
+
   if((0x04&Reg_value) == 4)
   {
     tone(buzzerPin,523,2000);
@@ -50,9 +58,7 @@ void loop(){
   {
     noTone(buzzerPin);
   }
- // delay(1000);
- //analogWrite(steer, map(analogRead(0),0,1023,0,255));
- //OCR1A = map(analogRead(0),0,1023,3000,6000);
+  
 }
 
 void Stop(void){
@@ -88,12 +94,3 @@ void Reverse(void){
   
 }
 
-//  TCCR2A  =0b11000010;
-//  TCCR2B  =0b00001101;
-//  OCR2A   =188;
-
-// for(int i=136;i<=230;i++){
-//   
-//   OCR2A = i; 
-//   delay(20);
-// }
