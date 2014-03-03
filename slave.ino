96f71db98fd7d58c5009073d799c5db85b31c8b7
fredthekid <fredthekid@gmail.com>
2014-03-02 17:01:31 -0800

    Update slave.ino

--- slave.ino
+++ slave.ino
@@ -3,94 +3,85 @@ SoftwareSerial mySerial (5, 6);
 
 const int front_LED [2] = {7, 8};
 const int back_LED [2] = {12, 13};
-
 const int buzzerPin = 3;
 
 void setup(){
-
    mySerial.begin(9600);
    Serial.begin(115200);
    
-   pinMode(3, OUTPUT);
-   pinMode(7,OUTPUT);
-   pinMode(8,OUTPUT);
-   pinMode(9,OUTPUT);
-   pinMode(10,OUTPUT);
-   pinMode(11,OUTPUT);
-   pinMode(12,OUTPUT);
+   pinMode(3, OUTPUT); //buzzer
+   pinMode(7,OUTPUT); //front led
+   pinMode(8,OUTPUT); //front led
+   pinMode(9,OUTPUT); //speed motor
+   pinMode(10,OUTPUT); //speed motor
+   pintMode(11,OUTPUT); //steering servo
+   pinMode(12,OUTPUT); //back led
+   pinMode(13,OUTPUT); //back led
    
-   TCCR2A  =0b11000010;
-   TCCR2B  =0b00001101;
-   OCR2A   =188;
-
-  TCCR1A	= 0b11110010;		// Clear OC1A/OC1B on compare match, set OC1A/OC1B at BOTTOM, Fast PWM mode
-  TCCR1B	= 0b00011010;		// input noise canceler, input capture rising edge, 1 prescale
-  ICR1	= 42000;     					// set period of PWM signal
-  
-  OCR1A	= 3000;					// set high time of output PWM signal
-  OCR1B	= 3000;					// set high time of output PWM signal
+   TCCR2A = 0b11000010;
+   TCCR2B = 0b00001101;
+   OCR2A = 188;
+   TCCR1A = 0b11110010;		// Clear OC1A/OC1B on compare match, set OC1A/OC1B at BOTTOM, Fast PWM mode
+   TCCR1B = 0b00011010;		// input noise canceler, input capture rising edge, 1 prescale
+   ICR1 = 42000;     		// set period of PWM signal
+   OCR1A = 3000;				// set high time of output PWM signal
+   OCR1B = 3000;				// set high time of output PWM signal
 }
 
 void loop(){
-
-  char Reg_value= mySerial.read();
-  Serial.println(Reg_value, BIN);
-  
+   char Reg_value= mySerial.read();
+   Serial.println(Reg_value, BIN);
    char accel = Reg_value&0x03;
    switch(accel)
-     {
-       case 1: Forward();
-               break;
-       case 2: Reverse();
-               break;
-       default:Stop();
-     }
-
+   {
+      case 1: Forward();
+              break;
+      case 2: Reverse();
+              break;
+      default:Stop();
+   }
+   
    char steer = Reg_value & 0x0F8;
-   steer>>3;
+   steer >> 3;
    OCR2A = map(steer, 0, 31, 136, 230);
-
-  if((0x04&Reg_value) == 4)
-  {
-    tone(buzzerPin,523,2000);
-  }
-  else
-  {
-    noTone(buzzerPin);
-  }
   
+   if((0x04&Reg_value) == 4)
+   {
+      tone(buzzerPin,523,2000);
+   }
+   else
+   {
+      noTone(buzzerPin);
+   }
 }
 
 void Stop(void){
-  OCR1A	= 3000;	
-  OCR1B	= 3000;	
- for(int i = 0; i<2; i++)
- {
- digitalWrite(front_LED[i], LOW);
- digitalWrite(back_LED[i], LOW);
- 
- }
-  
+  OCR1A	= 3000;
+  OCR1B	= 3000;
+  for(int i = 0; i<2; i++)
+  {
+    digitalWrite(front_LED[i], LOW);
+    digitalWrite(back_LED[i], LOW);
+  }
 }
 
 void Forward(void){
-  OCR1A	= 3400;	
-  OCR1B	= 2600;	
-   for(int i = 0; i<2; i++)
- {
- digitalWrite(front_LED[i], HIGH);
- digitalWrite(back_LED[i], LOW);
- }
+OCR1A	= 3400;	
+OCR1B	= 2600;	
+  for(int i = 0; i<2; i++)
+  {
+    digitalWrite(front_LED[i], HIGH);
+    digitalWrite(back_LED[i], LOW);
+  }
 }
 
 void Reverse(void){
-  OCR1A	= 2600;	
-  OCR1B	= 3400;	
+   OCR1A	= 2600;	
+   OCR1B	= 3400;	
    for(int i = 0; i<2; i++)
- {
- digitalWrite(back_LED[i], HIGH);
- digitalWrite(front_LED[i], LOW);
- }
-  
+   {
+     digitalWrite(back_LED[i], HIGH);
+     digitalWrite(front_LED[i], LOW);
+   }
 }
 
